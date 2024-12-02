224594f75627568e80a927df9074f6d43223de9e
Claudio <claudio@ccmdesign.ca>
2024-12-02 09:47:33 -0800

    component examples

--- components/blogSearch.vue
+++ components/blogSearch.vue
@@ -1,7 +1,7 @@
 <template>
   <fieldset class="search">
     <input type="search" />
-    <input type="submit" class="button" />
+    <input type="submit" />
   </fieldset>
 </template>
 
@@ -9,8 +9,31 @@
 
 <style lang="scss" scoped>
   .search {
+    --_search-border-radius: var(--base-border-radius);
+    --_search-padding: 0.5rem;
+  }
+
+  // Structure
+  .search {
     display: flex;
-    align-items: center;
+    align-items: stretch;
+  }
+
+  input[type="search"] {
+    // padding: 1rem;
+  }
+
+  // Skin
+  .search * {
     border: 0;
+    padding: var(--_search-padding);
+  }
+
+  input[type="search"] {
+    border-radius: var(--_search-border-radius) 0 0 var(--_search-border-radius);
+  }
+
+  input[type="submit"] {
+    border-radius: 0 var(--_search-border-radius) var(--_search-border-radius) 0;
   }
 </style>
