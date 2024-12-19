0b58278877b6974eb30f98e5f0f37f5c163f6e65
Claudio <claudio@ccmdesign.ca>
2024-12-18 17:40:00 -0800

    remapping soem util vars

--- components/blogChip.vue
+++ components/blogChip.vue
@@ -26,18 +26,24 @@
 
 <style lang="scss" scoped>
   .chip {
-    --_chip-bg: var(--base-color-10-tint);
-    --_chip-bg: red;
-    --_chip-color: var(--light-text-color);
+    --_chip-background: var(--base-color-100);
+    --_chip-color: var(--base-color-400);
     --_chip-padding: var(--space-3xs) var(--space-xs);
-    --_chip-border-color: var(--base-color-30-tint);
+    --_chip-border-color: var(--base-color-500);
+    --_chip-border-width: 1px;
+    --_chip-border-radius: 40px;
+    --_chip-font-size: var(--size--1);
+    --_chip-font-weight: 400;
+    --_chip-shadow: none;
   }
   .chip {
-    font-size: var(--size--1);
-    font-weight: 400;
-    background-color: var(--_chip-bg);
+    font-size: var(--_chip-font-size);
+    font-weight: var(--_chip-font-weight);
+    background: var(--_chip-background);
     color: var(--_chip-color);
     padding: var(--_chip-padding);
-    border-radius: 100px;
+    border-radius: var(--_chip-border-radius);
+    border: var(--_chip-border-width) solid var(--_chip-border-color);
+    box-shadow: var(--_chip-shadow);
   }
 </style>
