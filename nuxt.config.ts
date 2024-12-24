d952109d53ca7ae909dcb44e837f5d460bcd9ec6
Claudio <claudio@ccmdesign.ca>
2024-12-23 20:47:10 -0800

    initial setup for project_config.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,24 @@
 import { defineNuxtConfig } from "nuxt/config";
 import { resolve } from "path";
+import projectConfig from "./project_config.json";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   app: {
     head: {
-      title: "Blog Template",
+      title: projectConfig.title,
       meta: [
-        { name: "description", content: "Blog Template" },
+        { name: "description", content: projectConfig.description },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
-        { property: "og:site_name", content: "Blog Template" },
-        { property: "og:title", content: "Blog Template" },
-        { property: "og:description", content: "Blog Template" },
-        { property: "og:image", content: "/images/OG.jpg" },
-        { property: "og:image:alt", content: "Page image for Blog Template" },
-        { name: "twitter:image", content: "/images/OG.jpg" },
-        { name: "twitter:image:alt", content: "Blog Template" },
-        { name: "twitter:description", content: "Blog Template" },
+        { property: "og:site_name", content: projectConfig.title },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { hid: 'og:title', property: 'og:title', content: projectConfig.title },
+        { hid: 'og:description', property: 'og:description', content: projectConfig.description },
+        { hid: 'og:image', property: 'og:image', content: projectConfig.image },
+        { hid: 'og:url', property: 'og:url', content: projectConfig.url },
+        { name: "twitter:image", content: projectConfig.image },
+        { name: "twitter:image:alt", content: projectConfig.title },
+        { name: "twitter:description", content: projectConfig.title }
       ],
       script: [],
       link: [
