279e0ed95dca818c56f319e8dabf0899df1c8e41
Claudio <claudio@ccmdesign.ca>
2024-12-23 20:46:52 -0800

    blogPartners - WIP

--- components/blogPartners.vue
+++ components/blogPartners.vue
@@ -1,19 +1,30 @@
 <template>
   <blog-section>
     <div class="center">
-      <h4 split>Partners</h4>
-      <div class="cluster">
-        <img src="/images/govlab-logo.svg" alt="" />
-        <img src="/images/burnes-center-logo.svg" alt="" />
+      <h4 class="partners-title" split>Partners</h4>
+      <div class="cluster partners-cluster">
+        <img v-for="logo in logos" :key="logo.src" :src="logo.src" :alt="logo.alt" />
       </div>
     </div>
   </blog-section>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+  const props = defineProps<{
+    logos: Array<{ src: string; alt: string }>
+  }>();
+
+  const defaultLogos = [
+    { src: "/images/govlab-logo.svg", alt: "GovLab Logo" },
+    { src: "/images/burnes-center-logo.svg", alt: "Burnes Center Logo" },
+  ];
+
+  const logos = props.logos.length ? props.logos : defaultLogos;
+</script>
 
 <style lang="scss" scoped>
-  section {
+  .partners-title {
+    margin-block: 0;
   }
 
   .cluster {
@@ -21,12 +32,10 @@
     justify-content: center;
   }
 
-  h4 {
-    margin-block: 0;
-  }
-
   img {
-    max-width: 200px;
-    max-height: 44px;
+    --_partners-image-width: var(--partners-image-width);
+    --_partners-image-height: var(--partners-image-height);
+    max-width: var(--_partners-image-width);
+    max-height: var(--_partners-image-height);
   }
 </style>
