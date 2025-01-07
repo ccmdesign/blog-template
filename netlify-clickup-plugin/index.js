b86ca35ba2984f53a19fe4ce3de199feb628221f
Rafael Marques <rafamarques93@hotmail.com>
2025-01-07 16:33:41 -0300

    Add plugin notificacao ClickUp

---
+++ netlify-clickup-plugin/index.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+
+module.exports = {
+  onError: async ({ error, constants, utils }) => {
+    try {
+        const clickUpToken = process.env.CLICKUP_API_TOKEN; 
+        const chatViewId = process.env.CLICKUP_CHAT_VIEW_ID; 
+
+        if (!clickUpToken || !chatViewId) {
+            utils.build.failPlugin("CLICKUP_API_TOKEN ou CLICKUP_CHAT_VIEW_ID não configurados!");
+        }
+
+        const message = {
+            message: `🚨 **Deploy falhou no site ${constants.SITE_NAME}**\n\nErro: ${error.message}`,
+        };
+
+        const url = `https://api.clickup.com/api/v2/view/${chatViewId}/message`;
+
+        await axios.post(url, message, {
+            headers: {
+            Authorization: clickUpToken,
+            "Content-Type": "application/json",
+            },
+        });
+
+        console.log("Mensagem enviada ao canal de chat no ClickUp!");
+    } catch (err) {
+        utils.build.failPlugin("Erro ao enviar mensagem para o chat no ClickUp.", { error: err });
+    }
+  },
+};
