438694818e73d0c6f326be38dd3ae5d3db02036c
Claudio Mendonca <claudioccm@gmail.com>
2025-01-07 08:08:44 -0800

    hides partners on mobile

--- components/blogPartners.vue
+++ components/blogPartners.vue
@@ -1,5 +1,5 @@
 <template>
-  <blog-section :theme="theme">
+  <blog-section class="partners" :theme="theme">
     <div class="center">
       <div class="cluster partners-cluster">
         <h4 class="partners-title" split>Partners</h4>
@@ -51,10 +51,17 @@
     }
   }
 
+  @media screen and (max-width: 400px) {
+    .partners { display: none; }    
+  }
+
+
+
   img {
     // --_partners-image-width is being declared in the HTML section
     --_partners-image-height: var(--partners-image-height, auto);
     max-width: var(--_partners-image-width);
     max-height: var(--_partners-image-height);
+    width: 100%;
   }
 </style>
