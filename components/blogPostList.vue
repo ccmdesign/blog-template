c76a1684ad92eebfe013fa4ad5e97ec9ef963890
Claudio Mendonca <claudioccm@gmail.com>
2025-01-07 08:09:09 -0800

    adds more-posts to the blog-post-list

--- components/blogPostList.vue
+++ components/blogPostList.vue
@@ -1,8 +1,15 @@
 <template>
   <section class="blog-post-list | base-section" :content="content">
     <slot>
-      <div class="grid">
-        <blog-card :content="item" v-for="item in content" :key="item.slug" />
+      <div class="center stack">
+        
+        <div class="grid">
+          <blog-card :content="item" v-for="item in content" :key="item.slug" />
+        </div>
+        
+        <div class="text-align:center">
+          <nuxt-link class="button">More Posts</nuxt-link>
+        </div>
       </div>
     </slot>
   </section>
@@ -30,4 +37,6 @@ defineProps({
     padding-block: var(--_section-padding);
     padding-inline: var(--base-padding-inline);
   }
+
+
 </style>
